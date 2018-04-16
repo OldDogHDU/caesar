1df78f348cf46b3f2d93c95e610d8e9c8ee4454f
Your Name <you@example.com>
2018-04-16 17:13:09 +0800

    lwt

---
+++ decryption.php
@@ -0,0 +1,28 @@
+<?php
+$content=$_POST['content'];
+$key=$_POST['key'];
+$zifu=array(chr(33),chr(34),chr(35),chr(36),chr(37),chr(38),chr(39),chr(40),chr(41),chr(42),
+            chr(43),chr(44),chr(45),chr(46),chr(47),chr(58),chr(59),chr(60),chr(61),chr(62),
+            chr(63),chr(64),chr(91),chr(92),chr(93),chr(94),chr(95),chr(96),chr(123),chr(124),
+            chr(125),chr(126));
+for($i=0;$i<strlen($content);$i++)
+{ 
+	if($content[$i]>='a'&&$content[$i]<='z')
+	{
+		$content[$i]=chr(ord('a')+(ord($content[$i])-ord('a')-$key%26+26)%26);
+	}
+	elseif($content[$i]>='A'&&$content[$i]<='Z')
+	{
+		$content[$i]=chr(ord('A')+(ord($content[$i])-ord('A')-$key%26+26)%26);
+	}
+	elseif($content[$i]>='0'&&$content[$i]<='9')
+	{
+		$content[$i]='0'+($content[$i]-'0'-$key%10+10)%10;
+	}
+	else
+	{
+		$content[$i]=chr(33+(ord($content[$i])-33-$key%32+32)%32);
+	}
+}
+echo $content;
+?>
